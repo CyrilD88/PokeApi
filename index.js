32e1ded0d058f181fa916c252b7a680a2b423b93
Cyril Derc <cyril_derc@hotmail.com>
2023-08-20 19:11:52 +0200

    INDEX.JS - modification de la fonction changePokemon

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let button = document.getElementById("button");
+let image = document.getElementById("image");
+let pokeNumber = document.getElementById("number");
+let pokeName = document.getElementById("name");
 
-const changePokemon = () => {};
+const changePokemon = async () => {
+  let randomNumber = Math.ceil(Math.random() * 150) + 1;
+
+  let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+
+  let data = await fetch(requestString);
+  console.log(data);
+
+  let response = await data.json();
+  console.log(response);
+};
 
 button.addEventListener("click", changePokemon);
